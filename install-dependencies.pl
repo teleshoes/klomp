d60f1e4133455d5e8a811cb40ac3ccbbfaa27c53
Elliot Wolk <elliot.wolk@gmail.com>
2012-02-07 13:19:17 -0500

    pulled out the script copying from the dependency building

---
+++ install-dependencies.pl
@@ -0,0 +1,51 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+if(`whoami` ne "root\n"){
+  exec 'sudo', $0, @ARGV;
+}
+
+sub run(@){
+  print "@_\n";
+  system @_;
+}
+sub runPrompt(@){
+  print "@_\nrun the above cmd? [y/N]";
+  if(lc <STDIN> eq "y\n"){
+    system @_;
+  }
+}
+
+print "searching for and playing music:\n";
+run qw(apt-get install perl python mplayer sqlite3);
+
+print "\n\n";
+
+print "optional interactive CLI\n";
+runPrompt qw(cpan Term::ReadKey);
+
+print "\n\n";
+
+print "optional tag-reading-libs for:\n";
+print "\n\n-mp3 {eyed3}\n";
+runPrompt qw(apt-get install eyed3);
+
+print "\n\n-ogg and flac {lltag}:\n";
+runPrompt qw(apt-get install lltag);
+
+print "\n\n-wma {Audio::WMA}\n";
+runPrompt qw(cpan Audio::WMA);
+
+print "\n\n-mp4, m4a, m4p, m4v, m4b {AtomicParsley }\n";
+runPrompt qw(apt-get install atomicparsley);
+
+print "\n\n";
+
+print "japanese transliteration tagging {Lingua::JA::Romanize::Japanese}\n";
+runPrompt qw(cpan Lingua::JA::Romanize::Japanese);
+
+print "\n\n";
+
+print "flacmirror: flac=>ogg parallel-dir-structure syncing\n";
+runPrompt qw(apt-get install dir2ogg);
--- install.pl
+++ install.pl
@@ -23,55 +23,9 @@ my @execs = qw(
 );
 
 
-sub run(@){
-  print "@_\n";
-  system @_;
-}
-sub runPrompt(@){
-  print "@_\nrun the above cmd? [y/N]";
-  if(lc <STDIN> eq "y\n"){
-    system @_;
-  }
-}
-
-print "searching for and playing music:\n";
-run qw(apt-get install perl python mplayer sqlite3);
-
-print "\n\n";
-
-print "optional interactive CLI\n";
-runPrompt qw(cpan Term::ReadKey);
-
-print "\n\n";
-
-print "optional tag-reading-libs for:\n";
-print "\n\n-mp3 {eyed3}\n";
-runPrompt qw(apt-get install eyed3);
-
-print "\n\n-ogg and flac {lltag}:\n";
-runPrompt qw(apt-get install lltag);
-
-print "\n\n-wma {Audio::WMA}\n";
-runPrompt qw(cpan Audio::WMA);
-
-print "\n\n-mp4, m4a, m4p, m4v, m4b {AtomicParsley }\n";
-runPrompt qw(apt-get install atomicparsley);
-
-print "\n\n";
-
-print "japanese transliteration tagging {Lingua::JA::Romanize::Japanese}\n";
-runPrompt qw(cpan Lingua::JA::Romanize::Japanese);
-
-print "\n\n";
-
-print "flacmirror: flac=>ogg parallel-dir-structure syncing\n";
-runPrompt qw(apt-get install dir2ogg);
-
-print "\n\n";
-
 my $dest = '/usr/local/bin';
 print "copying the scripts to $dest\n";
 
 for my $exec(@execs){
-  run ("cp", $exec, $dest);
+  run "cp", $exec, $dest;
 }
