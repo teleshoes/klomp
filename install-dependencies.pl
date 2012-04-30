3a2a23156393b9a0d0835c37e04dda9845128117
Elliot Wolk <elliot.wolk@gmail.com>
2012-04-30 16:37:48 -0400

    use perl modules in the repo instead of from cpan where possible

--- install-dependencies.pl
+++ install-dependencies.pl
@@ -22,27 +22,29 @@ run qw(apt-get install perl python mplayer sqlite3);
 
 print "\n\n";
 
-print "optional interactive CLI\n";
-runPrompt qw(cpan Term::ReadKey);
+print "optional interactive CLI\n  {perl moudle Term::ReadKey}\n";
+runPrompt qw(apt-get install lib-term-readkey-perl);
 
 print "\n\n";
 
 print "optional tag-reading-libs for:\n";
+
 print "\n\n-mp3 {eyed3}\n";
 runPrompt qw(apt-get install eyed3);
 
 print "\n\n-ogg and flac {lltag}:\n";
 runPrompt qw(apt-get install lltag);
 
-print "\n\n-wma {Audio::WMA}\n";
-runPrompt qw(cpan Audio::WMA);
+print "\n\n-wma {perl module Audio::WMA}\n";
+runPrompt qw(apt-get install lib-audio-wma-perl);
 
 print "\n\n-mp4, m4a, m4p, m4v, m4b {AtomicParsley }\n";
 runPrompt qw(apt-get install atomicparsley);
 
 print "\n\n";
 
-print "japanese transliteration tagging {Lingua::JA::Romanize::Japanese}\n";
+print "japanese transliteration tagging\n";
+print "  {perl module Lingua::JA::Romanize::Japanese}\n";
 runPrompt qw(cpan Lingua::JA::Romanize::Japanese);
 
 print "\n\n";
