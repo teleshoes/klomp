ffc2be72c358bef9ede7e654864a8f822a7bdecf
Elliot Wolk <elliot.wolk@gmail.com>
2013-09-08 13:18:16 -0400

    duration: rename len=>time/duration

--- src/Klomp/Duration.pm
+++ src/Klomp/Duration.pm
@@ -1,13 +1,13 @@
 package Klomp::Duration;
 use Exporter 'import';
-@EXPORT_OK = qw(getLen formatLenS formatLenHMS);
+@EXPORT_OK = qw(getDuration formatTimeS formatTimeHMS);
 use strict;
 use warnings;
 
 sub selectExec(@);
-sub getLen($);
-sub formatLenS($);
-sub formatLenHMS($);
+sub getDuration($);
+sub formatTimeS($);
+sub formatTimeHMS($);
 
 sub selectExec(@){
   for my $cmd(@_){
@@ -17,7 +17,7 @@ sub selectExec(@){
   }
 }
 
-sub getLen($){
+sub getDuration($){
   my $file = shift;
   die "file not found: $file\n" unless -e $file;
 
@@ -30,19 +30,19 @@ sub getLen($){
   if($info =~ /Duration: (\d+):(\d+):(\d+(?:\.\d+))/){
     return $3 + ($2*60) + ($1*60*60);
   }else{
-    die "Unknown length for input: $file\n";
+    die "Unknown duration for input: $file\n";
   }
 }
 
-sub formatLenS($){
+sub formatTimeS($){
   return sprintf "%.2f", $_[0];
 }
-sub formatLenHMS($){
-  my $len = shift;
-  $len = int(0.5 + $len);
-  my $h = int($len / (60*60));
-  my $m = int($len % (60*60) / (60));
-  my $s = $len - ($m*60) - ($h*60*60);
+sub formatTimeHMS($){
+  my $time = shift;
+  $time = int(0.5 + $time);
+  my $h = int($time / (60*60));
+  my $m = int($time % (60*60) / (60));
+  my $s = $time - ($m*60) - ($h*60*60);
   $s = "0$s" if $s < 10;
 
   if($h == 0){
