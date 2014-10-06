2ce1c7df2ce070044539208165724fa5fd4c13f0
Elliot Wolk <elliot.wolk@gmail.com>
2014-10-06 01:54:19 -0400

    history: add sub that returns date added for every song in history

--- src/Klomp/History.pm
+++ src/Klomp/History.pm
@@ -3,12 +3,57 @@ use strict;
 use warnings;
 use Klomp::Lib;
 
+sub getDateCache();
 sub getFiles();
 sub getCommits();
 sub getCommitInfo($);
 sub getHistoryDir();
 sub checkHistoryDir();
 sub gitLibsRun(@);
+
+sub getDateCache(){
+  return undef if not checkHistoryDir;
+
+  my @files = getFiles();
+  my @commits = getCommits();
+
+  my %fileCommit;
+
+  my $commitInfo = {};
+  for my $c(@commits){
+    $$commitInfo{$c} = getCommitInfo $c;
+    for my $file(@{$$commitInfo{$c}{files}}){
+      next if $file !~ s/^("?)libs\//$1/;
+      if(not defined $fileCommit{$file}){
+        $fileCommit{$file} = $c;
+      }
+    }
+  }
+
+  my $cnt = 0;
+  for my $file(@files){
+    if(not defined $fileCommit{$file}){
+      print "$file\n";
+    }else{
+      $cnt++;
+    }
+  }
+
+  my $dateCache = {};
+
+  for my $file(@files){
+    my $path = $file;
+    if($path =~ /^".*"$/){
+      $path =~ s/^"//;
+      $path =~ s/"$//;
+      $path =~ s/\\"/"/g;
+      $path =~ s/\\\\/\\/g;
+    }
+    $$dateCache{$path} = $$commitInfo{$fileCommit{$file}}{date};
+  }
+
+  return $dateCache;
+}
 
 sub getFiles(){
   return gitLibsRun "ls-files";
