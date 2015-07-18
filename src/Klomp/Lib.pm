74a30f3a5ba7740c162b61f61b4479d41fa8f91f
Elliot Wolk <elliot.wolk@gmail.com>
2015-07-18 14:25:10 -0400

    klomplib: add all valid properties

--- src/Klomp/Lib.pm
+++ src/Klomp/Lib.pm
@@ -20,6 +20,17 @@ sub getLibArray($;$);
 sub checkPropName($);
 
 my $props = {
+  symlink       => 'dir where all lib subdirs are symlinked by klomp-update',
+  history       => 'git repo where klomp-update tracks database and filenames',
+  startCmd      => 'run when klomp-cmd starts klomplayer',
+  stopCmd       => 'run when klomp-cmd tries to kill klomplayer',
+  hardStartCmd  => 'run after startCmd, but only when previously stopped',
+  hardStopCmd   => 'run after stopCmd, but only when previously running',
+  playlistCmd   => 'run when klomp-cmd changes playlists',
+  updateCurCmd  => 'run by klomplayer every 20s while klompcur is updated',
+  renice        => 'renice klomplayer to this value',
+  targetUser    => 're-run klomp-cmd as this user using sudo su -c',
+  disallowPause => 'klomp-cmd stops instead of pauses, killing klomplayer',
 };
 my $okProps = join "|", sort keys %$props;
 
