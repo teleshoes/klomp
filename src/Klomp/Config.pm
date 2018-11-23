0276c531d864f8c31aeecab727e2d1db26bc784a
Elliot Wolk <elliot.wolk@gmail.com>
2018-11-22 22:24:02 -0500

    klomp[config]: whitespace

--- src/Klomp/Config.pm
+++ src/Klomp/Config.pm
@@ -12,18 +12,18 @@ sub readConfigFile(;$);
 sub checkPropName($);
 
 my $props = {
-  symlink       => 'dir where all lib subdirs are symlinked by klomp-update',
-  history       => 'git repo where klomp-update tracks database and filenames',
-  playerType    => 'music playing engine, either mplayer or qtcmdplayer',
-  startCmd      => 'run when klomp-cmd starts klomplayer',
-  stopCmd       => 'run when klomp-cmd tries to kill klomplayer',
-  hardStartCmd  => 'run after startCmd, but only when previously stopped',
-  hardStopCmd   => 'run after stopCmd, but only when previously running',
-  playlistCmd   => 'run when klomp-cmd changes playlists',
-  updateCurCmd  => 'run by klomplayer every 20s while klompcur is updated',
-  renice        => 'renice klomplayer to this value',
-  targetUser    => 're-run klomp-cmd as this user using sudo su -c',
-  disallowPause => 'klomp-cmd stops instead of pauses, killing klomplayer',
+  symlink        => 'dir where all lib subdirs are symlinked by klomp-update',
+  history        => 'git repo where klomp-update tracks database and filenames',
+  playerType     => 'music playing engine, either mplayer or qtcmdplayer',
+  startCmd       => 'run when klomp-cmd starts klomplayer',
+  stopCmd        => 'run when klomp-cmd tries to kill klomplayer',
+  hardStartCmd   => 'run after startCmd, but only when previously stopped',
+  hardStopCmd    => 'run after stopCmd, but only when previously running',
+  playlistCmd    => 'run when klomp-cmd changes playlists',
+  updateCurCmd   => 'run by klomplayer every 20s while klompcur is updated',
+  renice         => 'renice klomplayer to this value',
+  targetUser     => 're-run klomp-cmd as this user using sudo su -c',
+  disallowPause  => 'klomp-cmd stops instead of pauses, killing klomplayer',
 };
 my $okProps = join "|", sort keys %$props;
 
