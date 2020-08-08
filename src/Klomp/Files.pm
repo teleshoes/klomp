c49e01b06b4c119bfde8c0152b427b4140766aa7
Elliot Wolk <elliot.wolk@gmail.com>
2020-08-07 21:34:23 -0400

    klomp[files]: add file 'synonyms' explicitly as separate entries

--- src/Klomp/Files.pm
+++ src/Klomp/Files.pm
@@ -11,25 +11,20 @@ my $files = {
   cur          => "$ENV{HOME}/.klomp/cur",
   list         => "$ENV{HOME}/.klomp/list",
   history      => "$ENV{HOME}/.klomp/history",
+  hist         => "$ENV{HOME}/.klomp/history",
   datecache    => "$ENV{HOME}/.klomp/datecache",
   lib          => "$ENV{HOME}/.klomp/lib",
   config       => "$ENV{HOME}/.klomp/config",
   playlistname => "$ENV{HOME}/.klomp/playlist",
+  plname       => "$ENV{HOME}/.klomp/playlist",
   playlistdir  => "$ENV{HOME}/.klomp/list-",
+  pldir        => "$ENV{HOME}/.klomp/list-",
   bufferlog    => "/tmp/qtcmdplayer-buffer.log",
   fifo         => "/tmp/klomplayer_fifo",
   fifopidfile  => "/tmp/klomplayer_fifo_pid",
   termpidfile  => "/tmp/klomp_term_pid",
   pidfile      => "/tmp/klomplayer_pid",
 };
-my $synonyms = {
-  plname => 'playlistname',
-  pldir => 'playlistdir',
-  hist => 'history',
-};
-for my $key(keys %$synonyms){
-  $$files{$key} = $$files{$$synonyms{$key}};
-}
 
 sub klompFile($){
   my $path = $$files{$_[0]};
